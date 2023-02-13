8a1bb6423c22efdd39878a43646090ec994f0d99
Matthew Bardoe <mbardoe@gmail.com>
2023-02-13 10:03:32 -0500

    implemented RandomTournament

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import random
 
 from strategy import Strategy
-from tournament import Tournament
+from tournament import Tournament, RandomTournament
 
 
 # def strategy1(num_rounds, player1_history, player2_history):
@@ -14,8 +14,50 @@ from tournament import Tournament
 
 
 if __name__ == "__main__":
-    #tournament = Tournament([Strategy.jerk(), Strategy.jerk(), Strategy.jerk(), Strategy.random(), Strategy.random(), Strategy.niceguy(), Strategy.niceguy(),
-    #                         Strategy.titfortat(), Strategy.titfortat(), Strategy.titfortat(), Strategy.titfortat()], 50)
-    
+    def strategy1(num_rounds, player1_history, player2_history):
+        if num_rounds==1:
+            return "C"
+        else:
+            return player2_history[-1]
+
+    table1=Strategy("Table1", strategy1)
+
+    def strategy2(num_rounds, player1_history, player2_history):
+        return "D"
+
+
+
+    table2=Strategy("Table2", strategy2)
+
+    def strategy3(num_rounds, player1_history, player2_history):
+        return "D"
+
+    table3=Strategy("Table3", strategy3)
+
+    def strategy4(num_rounds, player1_history, player2_history):
+        if num_rounds==1:
+            return "C"
+        elif num_rounds%4==0:
+            return "D"
+        else:
+            return player2_history[-1]
+
+
+    table4=Strategy("Table4", strategy4)
+
+    tournament=RandomTournament([table1, table2, table3, table4], .9)
+    #tournament = RandomTournament([Strategy.jerk(), Strategy.jerk(), Strategy.jerk(), Strategy.random(), Strategy.random(), Strategy.niceguy(), Strategy.niceguy(),
+    #                         Strategy.titfortat(), Strategy.titfortat(), Strategy.titfortat(), Strategy.titfortat()], .8)
+
     tournament.play_tournament()
     tournament.report_scores()
+
+
+
+
+
+
+
+
+
+
--- tournament.py
+++ tournament.py
@@ -1,4 +1,8 @@
+from random import random
+
 from strategy import Strategy
+
+
 class Tournament():
 
     def __init__(self, strategies: list[Strategy], num_rounds: int):
@@ -41,7 +45,42 @@ class Tournament():
         for strategy in self.strategies:
             print(f"{strategy.name}: {strategy.score}")
 
-if __name__=="__main__":
+
+class RandomTournament(Tournament):
+
+    def __init__(self, strategies, prob_continue=0.5):
+        super().__init__(strategies, 1)
+        if prob_continue>1 or prob_continue<0:
+            raise ValueError("Probability must be between 0 and 1")
+        self.prob_continue = prob_continue
+
+    def play_tournament(self):
+        keys = list(self.strategies)
+        num_keys = len(keys)
+        for i in range(num_keys):
+            player1 = self.strategies[i]
+
+            for j in range(i + 1, num_keys):
+                player1_history = []
+                player2 = self.strategies[j]
+                player2_history = []
+                k = 0
+                cont = True
+                while cont:
+                    player1_outcome = player1.play(k + 1, player1_history, player2_history)
+                    player2_outcome = player2.play(k + 1, player2_history, player1_history)
+                    player1_history.append(player1_outcome)
+                    player2_history.append(player2_outcome)
+                    outcome = self.compute_payoffs(player1_outcome, player2_outcome)
+                    player1.update_score(outcome[0])
+                    player2.update_score(outcome[1])
+                    k += 1
+                    next_round=random()
+                    if next_round > self.prob_continue:
+                        cont = False
+
+
+if __name__ == "__main__":
     print(Tournament.compute_payoffs("C", "C"))
     print(Tournament.compute_payoffs("D", "C"))
     print(Tournament.compute_payoffs("C", "D"))
